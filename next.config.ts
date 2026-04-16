4681da1dfa6d9f833b8a1a117dd69f43884724dc
Rifki7284 <rifkiahmadrivera07@gmail.com>
2026-04-16 11:11:48 +0700

    fix: config error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
   images: {
     remotePatterns: [
       {
@@ -15,9 +14,6 @@ const nextConfig: NextConfig = {
     dangerouslyAllowSVG: true,
     unoptimized: process.env.NODE_ENV === "development",
   },
-=======
-  /* config options here */
->>>>>>> 6c2a4d3b773dcf9e8dc59aa56b570a6f91de8708
 };
 
 export default nextConfig;
