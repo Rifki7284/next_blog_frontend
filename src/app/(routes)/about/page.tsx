903a9f83571f3d1e38d584d7d3ff11c06a3ca391
Rifki7284 <rifkiahmadrivera07@gmail.com>
2026-04-22 16:14:37 +0700

    feat:seo about page

--- src/app/(routes)/about/page.tsx
+++ src/app/(routes)/about/page.tsx
@@ -2,31 +2,22 @@ import CoreValues from "@/components/molecules/core-values";
 import CTASection from "@/components/molecules/cta-section";
 import Purpose from "@/components/molecules/purpose";
 import { getAbout } from "@/features/articles/services/aboutServices";
-import { About } from "@/features/articles/types/about";
 import { Metadata } from "next";
-
+import { SITE_CONFIG } from "@/constant/app";
 export const metadata: Metadata = {
-  title: "About Us | Nama Website",
-  description:
-    "Pelajari lebih lanjut tentang perusahaan kami, visi, misi, dan tim yang membangun produk ini.",
-  keywords: ["about", "tentang kami", "company profile"],
+  title: `About | ${SITE_CONFIG.name}`,
+  description: SITE_CONFIG.description,
+  keywords: ["about", "tentang kami", SITE_CONFIG.name],
   openGraph: {
-    title: "About Us | Nama Website",
-    description: "Kenali lebih dekat perusahaan kami.",
-    url: "https://yourdomain.com/about",
-    siteName: "Nama Website",
-    images: [
-      {
-        url: "https://yourdomain.com/og-about.jpg",
-        width: 1200,
-        height: 630,
-      },
-    ],
+    title: `About | ${SITE_CONFIG.name}`,
+    description: SITE_CONFIG.description,
+    url: `${SITE_CONFIG.url}/about`,
+    siteName: SITE_CONFIG.name,
     locale: "id_ID",
     type: "website",
   },
   alternates: {
-    canonical: "https://yourdomain.com/about",
+    canonical: `${SITE_CONFIG.url}/about`,
   },
 };
 export const revalidate = false;
